cda29fffb50d345db01054219898e03abbd71cfc
Saurav-Navdhare <navdharesaurav2003@gmail.com>
2023-07-14 01:18:20 +0530

    Added Home route to redirect to docs

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Importing all the required modules
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
@@ -5,22 +6,27 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
+// Middlewares
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 
+// Importing all the routes
 const userRoutes = require('./routes/userRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 const commonRoutes = require('./routes/commonRoutes');
 
+// Using all the routes
 app.use("/api/v1", commonRoutes);
 app.use("/api/v1", userRoutes);
 app.use("/api/v1/admin", adminRoutes);
 
+// Default route
 app.get("/", (req, res)=>{
-    res.send("Hello world");
+    res.redirect("https://github.com/Saurav-Navdhare/CP-platform-backend/blob/master/README.md")
 })
 
+// Connecting to the database
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
